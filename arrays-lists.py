966f83269d7f1cbd4df6d0c8f0c52f23db9dcc16
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 15:32:05 +0100

    Update arrays-lists.py

--- arrays-lists.py
+++ arrays-lists.py
@@ -15,3 +15,15 @@ print(fruits[2])
 
 # Printing the second to the fifth element of the list.
 print(fruits[1:5])
+
+# Assigning the value of the second element of the list to the variable a.
+a = fruits[1] = 'orange'
+
+# Printing the string "This is a new list:" and the list fruits.
+print("This is a new list:", fruits)
+
+# Adding the string 'carrot' to the list fruits.
+b = fruits.append('carrot')
+
+# Printing the list of fruits.
+print(fruits)
