270f0ec11e559ba312746ee7275185ee79fd25b8
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 15:27:09 +0100

    Update arrays-lists.py

---
+++ arrays-lists.py
@@ -0,0 +1,14 @@
+# Creating a list of fruits.
+fruits = ['apple', 'banana', 'cherry']
+
+# Printing the list of fruits.
+print(fruits)
+
+# Printing the type of the variable fruits.
+print(type(fruits))
+
+# Printing the length of the list.
+print(len(fruits))
+
+# Printing the third element of the list.
+print(fruits[2])
