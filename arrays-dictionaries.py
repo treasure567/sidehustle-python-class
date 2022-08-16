91485a2f652fbfd1bbe96eecdd4f6ebed3a56112
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 14:32:31 +0100

    Update arrays-dictionaries.py

---
+++ arrays-dictionaries.py
@@ -0,0 +1,16 @@
+# Creating a dictionary called school.
+school = {
+    'name': 'Trenalyze',
+    'location': 'Lagos',
+    'population': 780,
+    'annual_revenus': 120000
+}
+
+# Printing the dictionary.
+print(school)
+
+# Printing the type of the variable school.
+print(type(school))
+
+# Printing the value of the key 'name' in the dictionary school.
+print(school['name'])
