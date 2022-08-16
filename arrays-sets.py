b012afe79a12dd58893c2378aeba44a01d8152f8
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 14:14:14 +0100

    Update arrays-sets.py

--- arrays-sets.py
+++ arrays-sets.py
@@ -7,7 +7,17 @@ print (fruits)
 # Printing the type of the variable fruits.
 print (type(fruits))
 
+# Iterating over the set and printing each element.
 for a in fruits:
     print (a)
+
+# Checking if the string 'apple' is in the set fruits.
+print ('apple' in fruits)
+
+# Adding the string 'pawpaw' to the set fruits.
+fruits.add('pawpaw')
+
+# Printing the set.
+print (fruits)
 
 
