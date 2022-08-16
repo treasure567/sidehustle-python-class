db3403aa0c6c5a463a149dc08ccb8b757b622b3e
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 15:24:11 +0100

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,17 @@
+x = None
+
+print(x)
+
+if 5 > 2: print("Five is greater than two!")
+
+b = "Hello, World!"
+
+print(b[2:5])
+
+x,y,z = 1,2,3
+
+print(z,x)
+
+thistuple = ("apple", "banana", "cherry")
+
+print(thistuple[-1])
