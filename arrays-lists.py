8ee7d409f824ea0d7a2707ab912070c5c5f7ad91
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 15:40:18 +0100

    Update arrays-lists.py

--- arrays-lists.py
+++ arrays-lists.py
@@ -33,3 +33,22 @@ fruits.remove('lemon')
 
 # It prints the list of fruits.
 print(fruits)
+
+# Creating an empty list.
+fruits3 = []
+
+# Creating a new list called fruits3 and adding the element 'carrot' to it.
+for i in fruits:
+    if 'carrot' in i:
+        fruits3.append(i)
+    else:
+        continue
+
+# It prints the list fruits3.
+print(fruits3)
+
+# Creating a new list called fruits4 and adding the element 'apple' to it using list comprehension.
+fruits4 = [i for i in fruits if "apple" in i]
+
+# It prints the list fruits4.
+print(fruits4)
