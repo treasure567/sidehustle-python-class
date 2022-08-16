54d52c734010a9b5dd77812aafb2f30f25e023b4
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 15:28:29 +0100

    Update arrays-lists.py

--- arrays-lists.py
+++ arrays-lists.py
@@ -1,5 +1,5 @@
 # Creating a list of fruits.
-fruits = ['apple', 'banana', 'cherry']
+fruits = ['apple', 'banana', 'cherry', 'lemon', 'pawpaw']
 
 # Printing the list of fruits.
 print(fruits)
@@ -12,3 +12,6 @@ print(len(fruits))
 
 # Printing the third element of the list.
 print(fruits[2])
+
+# Printing the second to the fifth element of the list.
+print(fruits[1:5])
