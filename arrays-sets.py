04d8c02e3ff40ae413b57602f2ccd2dcb3169b99
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-16 14:10:17 +0100

    Create arrays-sets.py

---
+++ arrays-sets.py
@@ -0,0 +1,13 @@
+# Creating a set.
+fruits = {'apple', 'banana', 'pineapple', 'sugarcane'}
+
+# Printing the set.
+print (fruits)
+
+# Printing the type of the variable fruits.
+print (type(fruits))
+
+for a in fruits:
+    print (a)
+
+
