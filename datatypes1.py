2f97c1d2117aac9d4ff2b6f64c3f90a814221d8e
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 20:58:21 +0100

    Create datatypes1.py

---
+++ datatypes1.py
@@ -0,0 +1,48 @@
+# Assigning the value "string" to the variable student_name.
+student_name = "string"
+
+# Printing the type of the variable student_name.
+print(type(student_name))
+
+# Assigning the value 2 to the variable integer.
+integer = 2
+
+# Printing the type of the variable integer.
+print(type(integer))
+
+# Assigning the value 34.2 to the variable flo.
+flo = 34.2
+
+# Printing the type of the variable flo.
+print(type(flo))
+
+# Assigning the value True to the variable boolean.
+boolean = True
+
+# Printing the type of the variable boolean.
+print(type(boolean))
+
+# Creating a dictionary with the keys "name", "age", "height", and "married" and assigning the values
+# "string", 2, 34.2, and True to those keys.
+dictionary = {"name": "string", "age": 2, "height": 34.2, "married": True}
+
+# Printing the type of the variable dictionary.
+print(type(dictionary))
+
+# Creating a list of numbers.
+lists = [1, 2, 3, 4, 5]
+
+# Printing the type of the variable lists.
+print(type(lists))
+
+# Creating a set of numbers.
+sets = {1, 2, 3, 4, 5}
+
+# Printing the type of the variable sets.
+print(type(sets))
+
+# Creating a tuple of numbers.
+tuples = (1, 2, 3, 4, 5)
+
+# Printing the type of the variable tuples.
+print(type(tuples))
