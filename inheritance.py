118894870cd7452d25ec743ab293aac5571bb0ab
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 23:29:30 +0100

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -24,20 +24,38 @@ y.Score("Treasure")
 # Calling the method Captain() from the class Player.
 y.Captain()
 
+# This class has a method called Add that takes two numbers and returns their sum
 class Solution1:
     def Add(self, num1, num2):
         return num1 + num2
 
+# This class is a solution to the problem of multiplying two numbers
 class Solution2:
     def Multiply(self, num1, num2):
         return num1 * num2
 
-class Solution3(Solution1):
+# This class inherits from Solution1 and overrides the Divide method to take user input and return the
+# result of the division
+class Solution3(Solution1, Solution2):
     def Divide(self, a, b):
         a = int(input("Enter first number: "))
         b = int(input("Enter second number: "))
         return (a / b)
 
+# Creating an object of the class Solution1.
 a = Solution1()
 
+# Calling the method Add() from the class Solution1 and passing the arguments 1 and 2.
 print(a.Add(1, 2))
+
+# Creating an object of the class Solution3.
+b = Solution3()
+
+# Calling the method Divide() from the class Solution3 and passing the arguments 3 and 2.
+print(b.Divide(3, 2))
+
+# Calling the method Add() from the class Solution1 and passing the arguments 4 and 5.
+print(b.Add(4, 5))
+
+# Calling the method Multiply() from the class Solution2 and passing the arguments 4 and 6.
+print(b.Multiply(4, 6))
