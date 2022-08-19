a9cd391a863c0dfdb179ab73f80ed7538c748301
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 21:06:14 +0100

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,14 @@
+# Assigning the value 45 to the variable a.
+a = 45
+
+# Printing the type of the variable a.
+print (type(str(a)))
+
+# Converting the value of a to an integer.
+print(type(int(a)))
+
+# Converting the value of a to a float.
+print(type(float(a)))
+
+# Converting the value of a to a boolean.
+print(type(bool(a)))
