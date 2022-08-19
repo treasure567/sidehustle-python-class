58201a4dbac0b96628860d860c9eb90fbe026f18
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 23:08:28 +0100

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,1 +1,14 @@
-t
+# The Employee class has a name attribute and a show method
+class Employee:
+    name = "John Doe"
+
+    def show(self):
+        print(self.name)
+
+
+
+# Creating an instance of the Employee class.
+employee = Employee()
+
+# Calling the show method on the employee object.
+employee.show()
