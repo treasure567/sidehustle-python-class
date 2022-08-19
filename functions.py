b443a1ff484486d47bd06562795f79a6e42d7d53
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 20:05:14 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -20,3 +20,20 @@ exampleFunction()
 
 # It prints the sum of 2 and 30
 print(add(2,30))
+
+def addNew(*args):
+    """
+    It takes three numbers as input, adds them together, and returns the result
+    
+    :param a: int
+    :param b: int = 0
+    :param c: int = 0
+    :return: The sum of the three numbers.
+    """
+    a = int(input('Enter first Number: '))
+    b = int(input('Enter second Number: '))
+    c = int(input('Enter third Number: '))
+    return a + b + c
+
+# Printing the sum of the three numbers.
+print("The total sum is: ", addNew('', ''))
