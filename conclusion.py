1a7ce8fdab3845103778bfa7b951136cb2fc3fda
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 20:41:17 +0100

    Update conclusion.py

--- conclusion.py
+++ conclusion.py
@@ -32,4 +32,21 @@ print(linear_search(arr, n, target))
 
 # Recursive approach
 def linear_search2(arr, index, target):
-    
+    if index == -1:
+        print("The array is exhausted")
+        return -1
+    if arr[index] == target:
+        print("Key Found", index)
+        return index
+    return linear_search2(arr, index - 1, target)
+
+# Creating an array of numbers.
+arr = [1,2,3,4,5,6,7,8,9,11,23,45,67,33,89,32,44,65,234,256,990,100]
+
+# Asking the user to input a number.
+target = int(input("Enter the target number: "))
+
+index = len(arr) - 1
+
+
+linear_search2(arr, index, target)
