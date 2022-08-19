39d2ff5e751323ed75a823e43aaa1f3a7def6a82
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 19:35:13 +0100

    Create array-tuple.py

---
+++ array-tuple.py
@@ -0,0 +1,18 @@
+# Creating a tuple.
+fruits = ('apple', 'banana', 'orange', 'cashew', 'pawpaw')
+
+# Printing the tuple.
+print(fruits)
+
+# Printing the type of the variable `fruits`.
+print(type(fruits))
+
+# Printing the length of the tuple.
+print(len(fruits))
+
+# Printing the third element in the tuple.
+print(fruits[2])
+
+# Printing the third element from the end of the tuple to the second element from the end of the
+# tuple.
+print(fruits[-3:-1])
