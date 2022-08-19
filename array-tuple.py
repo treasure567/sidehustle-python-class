dea793c0499a909c0cfabe87c0b41a813de1f87d
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 19:38:52 +0100

    Update array-tuple.py

--- array-tuple.py
+++ array-tuple.py
@@ -16,3 +16,15 @@ print(fruits[2])
 # Printing the third element from the end of the tuple to the second element from the end of the
 # tuple.
 print(fruits[-3:-1])
+
+#tuple does not support item assignment
+# fruits[1] = 'orange'
+
+# Converting the tuple `fruits` to a list.
+a = list(fruits)
+
+# Assigning the value `'watermelon'` to the second element in the list `a`.
+a[1] = 'watermelon'
+
+# Printing the list `a`.
+print(a)
