64e95dc43ddfdfcb466d52c72d2d8f625041de88
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 22:34:37 +0100

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,10 @@
+# Assigning the value "Treasure" to the variable first_name.
+first_name = "Treasure"
+
+# Assigning the value "Uvietobore" to the variable last_name.
+last_name = "Uvietobore"
+
+# Printing the value of the variable first_name.
+print(first_name)
+
+print(first_name[3])
