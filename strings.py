5b5669555f586876640313c9680584adbe06bcd8
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-19 22:36:51 +0100

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,5 +1,5 @@
 # Assigning the value "Treasure" to the variable first_name.
-first_name = "Treasure"
+first_name = "Treasure "
 
 # Assigning the value "Uvietobore" to the variable last_name.
 last_name = "Uvietobore"
@@ -7,4 +7,11 @@ last_name = "Uvietobore"
 # Printing the value of the variable first_name.
 print(first_name)
 
-print(first_name[3])
+# Printing the value of the variable first_name from index 3 to index 7.
+print(first_name[3:7])
+
+# Checking if the letter T is in the variable first_name.
+b = 'T' in first_name
+
+# Printing the value of the variable b.
+print(b)
