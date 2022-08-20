7ac11d4d99fd4d118318957c851dbc6609fa3426
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-21 00:03:16 +0100

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,8 @@
+# Importing the fibo module and renaming it as f.
+import fibo as f
+
+# Calling the function `fib` from the module `fibo` and passing the value `100` to it.
+f.fib(100)
+
+# Calling the function `fib2` from the module `fibo` and passing the value `100` to it.
+f.fib2(100)
