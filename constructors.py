f28c161328a0bcecbe41583b5c7c12ee7315ffd7
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-20 23:39:50 +0100

    Update constructors.py

--- constructors.py
+++ constructors.py
@@ -1,12 +1,28 @@
+# It creates a class called Employee.
 class Employee:
     def __init__(self, age, salary):
+        """
+        The __init__() function is called automatically every time the class is being used to create a
+        new object
+        
+        :param age: The age of the employee
+        :param salary: The salary of the employee
+        """
         self.age = age
         self.salary = salary
         print('This is a parametized constructor')
 
     def show(self, name):
+        """
+        The function show() takes in a parameter name and prints out the name, salary and age of the
+        employee
+        
+        :param name: This is the name of the employee
+        """
         print(f"The employeen name is {name}. The salary is {self.salary} and the age is {self.age}")
 
+# Creating an object called a of the class Employee.
 a = Employee(17, 20000)
 
+# Calling the function show() and passing in the parameter "Treasure"
 a.show("Treasure")
