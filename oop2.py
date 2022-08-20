17413c0b4c1a4a3dcc4f5a3faf865a5367c8130a
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-20 23:07:16 +0100

    Update oop2.py

--- oop2.py
+++ oop2.py
@@ -16,12 +16,19 @@ a.show()
 # abstraction is the ability of an object to be represented by a simpler interface.
 # Inhheritance is the ability of a class to inherit the attricutes and methods of another class.
 
+# The public method can access the private method, but the private method cannot be accessed directly
+# from outside the class.
 
+# It creates a class called Animal.
 class Animal:
     def __private(self):
         return 'This is a private Methof'
 
     def public(self):
+        """
+        The function public() is a member of the class Foo, and it prints the return value of the
+        function __private(), which is also a member of the class Foo
+        """
         print(self.__private())
 
 
