5edf193b9fafbbe6579f63115643520fb23574ff
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-08-20 23:45:28 +0100

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,8 +1,12 @@
+# Asking for two numbers and dividing them.
 try:
     a = int(input('Enter first Number: '))
     b = int(input('Enter second Number:' ))
     c = a / b
-except ZeroDivisionError:
-    print('Division by zero is not allowed')
-
-print(c)
+    print(c)
+# Catching the exception and printing the message.
+except Exception as e:
+    print('Sorry i can divide by 0')
+# Printing the last line of code.
+else:
+    print('Last Line of code')
